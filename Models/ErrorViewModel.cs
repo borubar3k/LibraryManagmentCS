b39bcc777b7279aa27180cec3009e9a39ef7f730
Konrad <borubar3k@gmail.com>
2024-07-03 12:45:59 +0200

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagmentCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
